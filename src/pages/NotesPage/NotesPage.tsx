112e4c2710da098ec4142256382bf097427aca7a
Felipe Barsaga Avila <philipatdev@gmail.com>
2021-10-11 16:23:35 +0300

    Layout making

--- src/pages/NotesPage/NotesPage.tsx
+++ src/pages/NotesPage/NotesPage.tsx
@@ -1,8 +1,17 @@
 import './NotesPage.scss';
+import Table from "../../components/Table/Table";
+import {nanoid} from "nanoid";
 
 const NotesPage = () => {
+    const noteItemsTableCols = ['Name', 'Created', 'Category', 'Content', 'Dates'];
+    const statItemsTableCols = ['Note Category', 'Active', 'Archived'];
     return(
-        <div></div>
+        <div className={ 'notes-page' }>
+            <Table columns={noteItemsTableCols} renderControllers={true} height={'400px'} key={nanoid()}/>
+            <Table columns={statItemsTableCols} renderControllers={false} key={nanoid()}>
+                <div>Hi</div>
+            </Table>
+        </div>
     )
 }
 
