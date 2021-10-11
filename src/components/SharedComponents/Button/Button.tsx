928275ee4e893ba4a5c3f4519f6dcc2a2be89b24
Felipe Barsaga Avila <philipatdev@gmail.com>
2021-10-11 17:37:18 +0300

    Creating common button component

--- src/components/SharedComponents/Button/Button.tsx
+++ src/components/SharedComponents/Button/Button.tsx
@@ -1,8 +1,21 @@
 import './Button.scss';
+import {FC} from "react";
 
-const Button = () => {
+type ButtonPropsType = {
+    type: 'submit' | 'button'
+    inner: string | HTMLOrSVGImageElement
+    bgColor: 'green' | 'blue'
+    color: string
+}
+
+const Button: FC<ButtonPropsType> = ({ type, inner, bgColor , color}) => {
     return(
-        <div></div>
+        <button className={ `common-btn ${ bgColor === 'green' ? 'common-btn--green' : 'common-btn--blue' }` }
+                type={ type }
+                style={{ color }}
+        >
+            { inner }
+        </button>
     )
 }
 
