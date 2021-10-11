ce01a9953f09fef03d18cd80a9d081d47d46c095
Felipe Barsaga Avila <philipatdev@gmail.com>
2021-10-11 16:23:04 +0300

    Creating Table component

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,8 +1,37 @@
 import './Table.scss';
+import {FC} from "react";
+import Column from "./Column/Column";
+import ControllerBtn, {ControllerBtnTypes} from "../SharedComponents/ControllerBtn/ControllerBtn";
+import {useLocation} from "react-router-dom";
+import {nanoid} from "nanoid";
 
-const Table = () => {
+type TableItemPropsType = {
+    columns: string[]
+    renderControllers: boolean
+    height?: string
+}
+const Table: FC<TableItemPropsType> = ({ columns, renderControllers = false, height='100%', children }) => {
+    const cols = columns.map(colName => <Column columnName={colName} key={nanoid()}/>);
+    const location = useLocation();
+    const controllersTypes: ControllerBtnTypes[] = [location.pathname === '/active' ? 'Archive' : 'Unzip', 'Delete'];
+    const controllers = renderControllers
+        ? controllersTypes.map((type) => {
+        return <ControllerBtn type={type}
+                              color={'white'}
+                              isAll={true}
+                              key={nanoid()}/>
+        })
+        : null;
     return(
-        <div></div>
+        <div className={ 'table' } >
+            <div className={ 'table__header' }>
+                { cols }
+                <div className={ 'table__controllers' }>{ controllers }</div>
+            </div>
+            <div className={ 'table__items' } style={{ height: height}}>
+                { children }
+            </div>
+        </div>
     )
 }
 
