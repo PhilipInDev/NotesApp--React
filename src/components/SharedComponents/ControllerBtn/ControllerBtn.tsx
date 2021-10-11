698efaa205efc9fa764cba46676ec2b9251600b6
Felipe Barsaga Avila <philipatdev@gmail.com>
2021-10-11 16:22:35 +0300

    Creating ConrollerBtn component

--- src/components/SharedComponents/ControllerBtn/ControllerBtn.tsx
+++ src/components/SharedComponents/ControllerBtn/ControllerBtn.tsx
@@ -1,8 +1,37 @@
 import './ControllerBtn.scss';
+import {FC} from "react";
 
-const ControllerBtn = () => {
+export type ControllerBtnTypes = 'Archive' | 'Unzip' | 'Edit' | 'Submit' | 'Delete';
+type ControllerBtnProps = {
+    type: ControllerBtnTypes
+    color: string
+    isAll: boolean
+}
+const ControllerBtn: FC<ControllerBtnProps> = ({ type, color, isAll }) => {
+    let iconClasses;
+    switch (type) {
+        case 'Archive':
+            iconClasses = 'fas fa-box';
+            break;
+        case 'Unzip':
+            iconClasses = 'fas fa-box-open';
+            break;
+        case 'Edit':
+            iconClasses = 'fas fa-pencil-alt';
+            break;
+        case 'Submit':
+            iconClasses = 'fas fa-check-circle';
+            break;
+        case 'Delete':
+            iconClasses = 'fas fa-trash-alt';
+            break
+        default:
+            iconClasses = 'far fa-question-circle'
+    }
     return(
-        <div></div>
+        <div className={'controller-btn'} title={isAll ? type + ' All' : type} style={{ color: color }}>
+            <i className={iconClasses}/>
+        </div>
     )
 }
 
