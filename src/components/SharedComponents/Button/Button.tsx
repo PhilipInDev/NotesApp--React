752b12cc3717d374a490fc3123cfed7c10e13c85
Felipe Barsaga Avila <philipatdev@gmail.com>
2021-10-13 22:41:01 +0300

    Adding disabled and onClick props

--- src/components/SharedComponents/Button/Button.tsx
+++ src/components/SharedComponents/Button/Button.tsx
@@ -1,18 +1,21 @@
 import './Button.scss';
-import {FC} from "react";
+import {FC, MouseEventHandler, ReactElement} from "react";
 
 type ButtonPropsType = {
     type: 'submit' | 'button'
-    inner: string | HTMLOrSVGImageElement
+    inner: string | ReactElement
     bgColor: 'green' | 'blue'
     color: string
+    disabled?: boolean
+    onClick: MouseEventHandler<HTMLButtonElement>
 }
 
-const Button: FC<ButtonPropsType> = ({ type, inner, bgColor , color}) => {
+const Button: FC<ButtonPropsType> = ({ type, inner, bgColor , color, disabled, onClick}) => {
     return(
-        <button className={ `common-btn ${ bgColor === 'green' ? 'common-btn--green' : 'common-btn--blue' }` }
+        <button className={ `common-btn ${ bgColor === 'green' ? 'common-btn--green' : 'common-btn--blue' } common-btn${disabled ? '--disabled' : ''}`}
                 type={ type }
                 style={{ color }}
+                onClick={onClick}
         >
             { inner }
         </button>
