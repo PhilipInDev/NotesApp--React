cfaf84a3197570f5a68754b30ed41070a4526788
Felipe Barsaga Avila <philipatdev@gmail.com>
2021-10-13 22:41:23 +0300

    Adding ref and error props

--- src/components/TableItems/Field/Field.tsx
+++ src/components/TableItems/Field/Field.tsx
@@ -1,28 +1,37 @@
 import './Field.scss';
-import {FC} from "react";
+import {FC, Ref} from "react";
 
 type InputSettings = {
     name?: string
     value?: string
     placeholder?: string
+    error?: string
     onChange?: any
 }
 type FieldPropsType = InputSettings & {
     innerText?: string
     isEditable: boolean
+    fieldRef?: Ref<any> | null
 }
 
-const Field: FC<FieldPropsType> = ({ innerText, isEditable,name, value, placeholder,onChange}) => {
+const Field: FC<FieldPropsType> = ({ innerText, isEditable, name, value, placeholder, error, fieldRef = null, onChange}) => {
     return(
         <div className={ 'table-item__field' }>
             {
-                isEditable && value
-                ? <input type={ 'text' }
-                         name={name}
-                         defaultValue={value ? value : ''}
-                         placeholder={placeholder ? placeholder : ''}
-                         onChange={onChange} />
-                : <p title={innerText}>{ innerText }</p>
+                isEditable
+                ? <>
+                        <input type={ 'text' }
+                               name={name}
+                               defaultValue={value ? value : ''}
+                               placeholder={placeholder ? placeholder : ''}
+                               className={error ? 'table-item__input-error' : ''}
+                               ref={fieldRef}
+                               onChange={onChange} />
+                        <span className="table-item__field-error">
+                            { error ? error : '' }
+                        </span>
+                    </>
+                : <p ref={fieldRef} title={innerText}>{ innerText }</p>
             }
         </div>
     )
