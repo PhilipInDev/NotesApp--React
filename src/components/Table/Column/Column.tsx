f846f8876cae8ee2327292ff1f8239310a2a3250
philip <philipatdev@gmail.com>
2021-12-01 11:31:30 +0200

    Updates

--- src/components/Table/Column/Column.tsx
+++
@@ -1,9 +0,0 @@
-const Column = (props: { columnName: string }) => {
-    return (
-        <div className={'table__col'}>
-            <h3>{props.columnName}</h3>
-        </div>
-    )
-}
-
-export default Column;
